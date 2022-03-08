61dc5f57eace32ef9c1763f9ed2194c90de3ac6a
Elbachir <elbachir.boukhlouf@cerence.com>
2022-03-09 00:15:05 +0100

    ignore test failure and generate. change driver path

--- src/test/java/Steps/StepDefs.java
+++ src/test/java/Steps/StepDefs.java
@@ -19,7 +19,7 @@ public class StepDefs {
 
     @Given("I navigate to the login page")
     public void iNavigateToTheLoginPage() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\Drivers\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://b2c.zeiss.com/b2c.zeiss.com/oauth2/v2.0/authorize?p=b2c_1a_zeissidsocialv2signin&client_id=db167f2e-fbee-48fb-acb2-0c65daff36a0&redirect_uri=https%3A%2F%2Fid-ip.zeiss.com%2FOAuth%2FAuthorizeCallBack&response_type=id_token&scope=openid+profile+offline_access&response_mode=form_post&nonce=637822829564737359.NWZiYWE0N2EtMTY3My00N2FiLWE4MDQtYjE1OWIzYWM2YTQ5NTllOWZiMjctMGY4MC00ZGYxLTk0NTEtYzMxZGMwMDk3M2U4&ui_locales=en&state=CfDJ8Mfbt-svviFEncIY2VZm1xUlh8EOSj_O27r5lIhEiWusDerJeOSBRqFD6q9BE-AN_lOaO206WXMggwKiZ3efMOKxX_VqcGQa789xvESRYYy9f50Ge5cVqtdpBPJR_pEezI9KJ8HM2c4OB_7uYpVrrStf0mSagbFSBK8GKliJnMQTyLBdW6YaiUD_qbwjuwvTPm6jo88i0h9lfg8L9hla9w9F8SCDNLWTlk_wvjJ_sIIILfMsXlqzaPglI-AlorDQEskFsjpq8WsXI0OOYOOoREZSU1nhO3O1eblCnfCG3jBs&x-client-SKU=ID_NETSTANDARD2_0&x-client-ver=5.5.0.0");
     }
